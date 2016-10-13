a2cb5862705839cc010e431d130c25a16c3a2837
Student <Student>
2016-10-13 10:53:03 -0400

    Improved vmupdate script

--- hol3548/vmupdate/vmupdate.bat
+++ hol3548/vmupdate/vmupdate.bat
@@ -1,2 +1,2 @@
-xcopy files\* c:\* /s /y
-copy ..\src\builder\.env %GOPATH%\bin /y
+xcopy %GOPATH%\src\github.com\hubert-heijkers\wow2016\hol3548\vmupdate\files\* c:\* /s /y
+copy %GOPATH%\src\github.com\hubert-heijkers\wow2016\hol3548\src\builder\.env %GOPATH%\bin /y
